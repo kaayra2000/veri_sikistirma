b133cc982b5195398dda27ffff45a43a31405905
muhammed kayra <kayrabulut39@gmail.com>
2023-10-10 15:59:43 +0300

    ödev bitti

--- veri_sikistirma.py
+++ veri_sikistirma.py
@@ -1,18 +1,59 @@
 from PIL import Image
 import sys
 
-def rle_encode(data):
-    runs = []
-    run_length = 1
-    for i in range(1, len(data)):
-        if data[i] == data[i-1]:
-            run_length += 1
-        else:
-            runs.append((data[i-1], run_length))
-            run_length = 1
-    runs.append((data[-1], run_length))
-    return runs
 
+def rle_decode(f):
+
+    # Geriye kalan veriyi bytes olarak oku
+    byte_data = f.read()
+
+    decoded_data = []
+    i = 0
+    while i < len(byte_data):
+        if byte_data[i] == 255:
+            count = byte_data[i+1]
+            value = byte_data[i+2]
+            if value == 254:
+                value = 255
+            decoded_data.extend([value] * count)
+            i += 3
+        else:
+            decoded_data.append(byte_data[i])
+            i += 1
+
+    return decoded_data
+
+def rle_encode(data):
+    encoded = []
+    i = 0
+    while i < len(data):
+        if i + 2 < len(data) and data[i] == data[i+1] == data[i+2]:
+            count = 3
+            while i + count < len(data) and data[i] == data[i+count]:
+                count += 1
+            while count >= 255:
+                encoded.append(255)
+                encoded.append(254)
+                count -= 254
+                if data[i] == 255:
+                    encoded.append(254)
+                else:
+                    encoded.append(data[i])
+                i += 254
+            encoded.append(255)
+            encoded.append(count)
+            if data[i] == 255:
+                encoded.append(254)
+            else:
+                encoded.append(data[i])
+            i += count
+        else:
+            if data[i] == 255:
+                encoded.append(254)
+            else:
+                encoded.append(data[i])
+            i += 1
+    return encoded
 def rle_image_compress_row_row(img_path, output_path):
     image = Image.open(img_path)
     width, height = image.size
@@ -23,50 +64,48 @@ def rle_image_compress_row_row(img_path, output_path):
         encoded_channels = []
         for channel in channels:
             encoded_channels.append(rle_encode(list(channel.getdata())))
-        with open(output_path, 'w') as f:
-            f.write(f"{width} {height} {image.mode}\n")
+        with open(output_path, 'wb') as f:
+            f.write(f"{width} {height} {image.mode}\n".encode('utf-8'))
             for ec in encoded_channels:
                 for pair in ec:
-                    f.write(f"{pair[0]} {pair[1]} ")
-                f.write('\n')
+                    f.write(int.to_bytes(pair, byteorder='big', length=1))
     elif image.mode in ['L', '1']:  # L: Gray scale, 1: Black & White
         encoded = rle_encode(list(image.getdata()))
-        with open(output_path, 'w') as f:
-            f.write(f"{width} {height} {image.mode}\n")
-            for pair in encoded:
-                f.write(f"{pair[0]} {pair[1]} ")
+        with open(output_path, 'wb') as f:
+            f.write(f"{width} {height} {image.mode}\n".encode('utf-8'))
+            for en in encoded:
+                f.write(int.to_bytes(en, byteorder='big', length=1))
     else:
         print(f'{image.mode} bulunamadı')
 
 
-def rle_decode(runs, width, height):
-    output = []
-    for value, count in runs:
-        output.extend([value] * count)
-    return output
-
 def rle_image_decompress_row_row(rle_path):
-
-    with open(rle_path, 'r') as f:
-        width, height, img_mode = f.readline().strip().split()
+    with open(rle_path, 'rb') as f:
+        width, height, img_mode = f.readline().decode('utf-8').strip().split()
         width, height = int(width), int(height)
         img_size = (width, height)
+        img_len = width * height
         if img_mode == 'RGB':
             channels_data = []
-            for _ in range(3):  # R, G, B kanalları için
-                line = f.readline().strip().split()
-                runs = [(int(line[i]), int(line[i+1])) for i in range(0, len(line), 2)]
-                decoded_channel = rle_decode(runs, width, height)
-                img_channel = Image.new('L', img_size)
-                img_channel.putdata(decoded_channel)
-                channels_data.append(img_channel)
+            all_colors = rle_decode(f)
+            R_C = all_colors[0:img_len]
+            G_C = all_colors[img_len:2*img_len]
+            B_C = all_colors[2*img_len:] 
+            img_channel_R = Image.new('L', img_size)
+            img_channel_G = Image.new('L', img_size)
+            img_channel_B = Image.new('L', img_size)
+            img_channel_R.putdata(R_C)
+            img_channel_G.putdata(G_C)
+            img_channel_B.putdata(B_C)
+            channels_data.append(img_channel_R)
+            channels_data.append(img_channel_G)
+            channels_data.append(img_channel_B)
             image = Image.merge('RGB', channels_data)
         else:  # Gray Scale veya Black & White için
-            line = f.readline().strip().split()
-            runs = [(int(line[i]), int(line[i+1])) for i in range(0, len(line), 2)]
-            data = rle_decode(runs, width, height)
+            data = rle_decode(f)
             image = Image.new(img_mode, img_size)
             image.putdata(data)
+
     image.show()
     return image
 
@@ -86,24 +125,23 @@ def rle_image_compress_column_column(img_path, output_path):
         encoded_channels = []
         for channel in channels:
             encoded_channels.append(rle_encode(list(column_data(channel))))
-        with open(output_path, 'w') as f:
-            f.write(f"{width} {height} {image.mode}\n")
+        with open(output_path, 'wb') as f:
+            f.write(f"{width} {height} {image.mode}\n".encode('utf-8'))
             for ec in encoded_channels:
                 for pair in ec:
-                    f.write(f"{pair[0]} {pair[1]} ")
-                f.write('\n')
+                        f.write(int.to_bytes(pair, byteorder='big', length=1))
     elif image.mode in ['L', '1']:  # L: Gray scale, 1: Black & White
         encoded = rle_encode(list(column_data(image)))
-        with open(output_path, 'w') as f:
-            f.write(f"{width} {height} {image.mode}\n")
-            for pair in encoded:
-                f.write(f"{pair[0]} {pair[1]} ")
+        with open(output_path, 'wb') as f:
+            f.write(f"{width} {height} {image.mode}\n".encode('utf-8'))
+            for en in encoded:
+                f.write(int.to_bytes(en, byteorder='big', length=1))
     else:
         print(f'{image.mode} bulunamadı')
 
 def rle_image_decompress_column_column(rle_path):
-    with open(rle_path, 'r') as f:
-        width, height, img_mode = f.readline().strip().split()
+    with open(rle_path, 'rb') as f:
+        width, height, img_mode = f.readline().decode('utf-8').strip().split()
         width, height = int(width), int(height)
         img_size = (width, height)
 
@@ -114,21 +152,28 @@ def rle_image_decompress_column_column(rle_path):
                 for y in range(image.height):
                     pixels[y * image.width + x] = next(data_iter)
             image.putdata(pixels)
-
+        img_len = width * height
         if img_mode == 'RGB':
             channels_data = []
-            for _ in range(3):  # R, G, B channels
-                line = f.readline().strip().split()
-                runs = [(int(line[i]), int(line[i+1])) for i in range(0, len(line), 2)]
-                channels_data.append(rle_decode(runs, width ,height))
+            all_colors = rle_decode(f)
+            R_C = all_colors[0:img_len]
+            G_C = all_colors[img_len:2*img_len]
+            B_C = all_colors[2*img_len:] 
+            img_channel_R = Image.new('L', img_size)
+            img_channel_G = Image.new('L', img_size)
+            img_channel_B = Image.new('L', img_size)
+            img_channel_R.putdata(R_C)
+            img_channel_G.putdata(G_C)
+            img_channel_B.putdata(B_C)
+            channels_data.append(img_channel_R)
+            channels_data.append(img_channel_G)
+            channels_data.append(img_channel_B)
             images = [Image.new('L', img_size) for _ in channels_data]
-            for image, data in zip(images, channels_data):
+            for image, data in zip(images, [R_C, G_C, B_C]):
                 set_column_data(image, data)
             image = Image.merge('RGB', images)
         else:  # Gray Scale or Black & White
-            line = f.readline().strip().split()
-            runs = [(int(line[i]), int(line[i+1])) for i in range(0, len(line), 2)]
-            data = rle_decode(runs, width, height)
+            data = rle_decode(f)
             image = Image.new(img_mode, img_size)
             set_column_data(image, data)
 
@@ -185,25 +230,27 @@ def rle_image_compress_zigzag(img_path, output_path):
         matrix = [list(image.getdata())[i * width: (i + 1) * width] for i in range(height)]
         return zigzag_flatten(matrix)
 
-    with open(output_path, 'w') as f:
-        f.write(f"{width} {height} {image.mode}\n")
+    with open(output_path, 'wb') as f:
+        f.write(f"{width} {height} {image.mode}\n".encode('utf-8'))
         if image.mode == 'RGB':
             channels = image.split()
+            encoded_channels = []
             for channel in channels:
-                data = get_zigzag_data(channel)
-                encoded = rle_encode(data)
-                for pair in encoded:
-                    f.write(f"{pair[0]} {pair[1]} ")
-                f.write('\n')
+                encoded_channels.append(rle_encode(list(get_zigzag_data(channel))))
+            with open(output_path, 'wb') as f:
+                f.write(f"{width} {height} {image.mode}\n".encode('utf-8'))
+                for ec in encoded_channels:
+                    for pair in ec:
+                            f.write(int.to_bytes(pair, byteorder='big', length=1))
         else:  # Gray Scale or Black & White
             data = get_zigzag_data(image)
             encoded = rle_encode(data)
-            for pair in encoded:
-                f.write(f"{pair[0]} {pair[1]} ")
+            for en in encoded:
+                f.write(int.to_bytes(en, byteorder='big', length=1))
 
 def rle_image_decompress_zigzag(rle_path):
-    with open(rle_path, 'r') as f:
-        width, height, img_mode = f.readline().strip().split()
+    with open(rle_path, 'rb') as f:
+        width, height, img_mode = f.readline().decode('utf-8').strip().split()
         width, height = int(width), int(height)
         img_size = (width, height)
 
@@ -211,21 +258,28 @@ def rle_image_decompress_zigzag(rle_path):
             matrix = zigzag_reshape(data, height, width)
             flat_data = [pixel for row in matrix for pixel in row]
             image.putdata(flat_data)
-
+        img_len = width * height
         if img_mode == 'RGB':
             channels_data = []
-            for _ in range(3):  # R, G, B channels
-                line = f.readline().strip().split()
-                runs = [(int(line[i]), int(line[i+1])) for i in range(0, len(line), 2)]
-                channels_data.append(rle_decode(runs, width, height))
+            all_colors = rle_decode(f)
+            R_C = all_colors[0:img_len]
+            G_C = all_colors[img_len:2*img_len]
+            B_C = all_colors[2*img_len:] 
+            img_channel_R = Image.new('L', img_size)
+            img_channel_G = Image.new('L', img_size)
+            img_channel_B = Image.new('L', img_size)
+            img_channel_R.putdata(R_C)
+            img_channel_G.putdata(G_C)
+            img_channel_B.putdata(B_C)
+            channels_data.append(img_channel_R)
+            channels_data.append(img_channel_G)
+            channels_data.append(img_channel_B)
             images = [Image.new('L', img_size) for _ in channels_data]
-            for image, data in zip(images, channels_data):
+            for image, data in zip(images, [R_C, G_C, B_C]):
                 set_zigzag_data(image, data)
             image = Image.merge('RGB', images)
         else:  # Gray Scale or Black & White
-            line = f.readline().strip().split()
-            runs = [(int(line[i]), int(line[i+1])) for i in range(0, len(line), 2)]
-            data = rle_decode(runs, width, height)
+            data = rle_decode(f)
             image = Image.new(img_mode, img_size)
             set_zigzag_data(image, data)
 
